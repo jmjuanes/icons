1d8f7d2bb1859fa111ee571593e8fcc7d90e39f7
Josemi <josemijuanes@gmail.com>
2023-01-24 21:11:00 +0100

    wip: added icons update tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,51 @@ const rename = require("gulp-rename");
 const through = require("through2");
 const Vinyl = require("vinyl");
 
-const processIcons = () => {
-        
+const cleanIcons = () => {
+    return through.obj(function (file, enc, callback) {
+        const iconPath = file.contents.toString().match(/\sd="([^\"]*)"/im)[1];
+        const content = [
+            `<svg viewBox="0 0 24 24" width="24" height="24" xmlns="http://www.w3.org/2000/svg">`,
+            `<g stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">`,
+            `<path d="${iconPath}" />`,
+            `</g>`,
+            `</svg>`,
+        ];
+        this.push(new Vinyl({
+            path: path.join(process.cwd(), path.basename(file.path)),
+            contents: new Buffer.from(content.join("")),
+        }));
+        return callback();
+    });
+};
+
+const generateMetadata = () => {
+    let unicode = 57343;
+    let metadata = {};
+    if (fs.existsSync("metadata.json")) {
+        metadata = JSON.parse(fs.readFileSync("metadata.json", "utf8"));
+        unicode = Math.max.apply(Object.keys(metadata).map(item => item.unicode));
+    }
+    const bufferContents = function (file, enc, callback) {
+        const name = path.basename(file.path, ".svg");
+        if (!metadata[name]) {
+            metadata[name] = {
+                name: name,
+                keywords: [],
+                unicode: unicode + 1,
+            };
+            unicode = unicode + 1;
+        }
+        return callback();
+    };
+    const endStream = function (callback) {
+        this.push(new Vinyl({
+            path: path.join(process.cwd(), "metadata.json"),
+            contents: new Buffer.from(JSON.stringify(metadata, null, "    ")),
+        }));
+        return callback();
+    };
+    return through.obj(bufferContents, endStream);
 };
 
 const iconsToJson = () => {
@@ -34,7 +77,17 @@ const iconsToJson = () => {
     return through.obj(bufferContents, endStream);
 };
 
-gulp.task("clean", () => null);
+gulp.task("update:icons-svg", () => {
+    return gulp.src("raw-icons/**/*.svg")
+        .pipe(cleanIcons())
+        .pipe(gulp.dest("icons"));
+});
+
+gulp.task("update:icons-metadata", () => {
+    return gulp.src("icons/*.svg")
+        .pipe(generateMetadata())
+        .pipe(gulp.dest("."));
+});
 
 gulp.task("build:json", () => {
     return gulp.src("icons/*.svg")
