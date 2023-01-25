fe436b803bd472014e48ac890538397cb299c30a
Josemi <josemijuanes@gmail.com>
2023-01-25 21:23:46 +0100

    chore: add task to generate icons for react

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,27 @@ const fs = require("fs");
 const path = require("path");
 const gulp = require("gulp");
 const rename = require("gulp-rename");
+const babel = require("gulp-babel");
 const through = require("through2");
 const Vinyl = require("vinyl");
+const hbs = require("handlebars");
+
+const pkg = require("./package.json");
+
+// Register handlebars helpers
+hbs.registerHelper("capitalizeFirst", str => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+});
+hbs.registerHelper("pascalCase", str => {
+    return str.match(/[a-zA-Z0-9]+/g).map(w => `${w.charAt(0).toUpperCase()}${w.slice(1)}`).join("");
+});
+
+// Tiny utility to extrack the d="" segment from the icon
+const getIconPath = str => str.match(/\sd="([^\"]*)"/im)[1];
 
 const cleanIcons = () => {
     return through.obj(function (file, enc, callback) {
-        const iconPath = file.contents.toString().match(/\sd="([^\"]*)"/im)[1];
+        const iconPath = getIconPath(file.contents.toString());
         const content = [
             `<svg viewBox="0 0 24 24" width="24" height="24" xmlns="http://www.w3.org/2000/svg">`,
             `<g stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">`,
@@ -24,11 +39,11 @@ const cleanIcons = () => {
 };
 
 const generateMetadata = () => {
-    let unicode = 57343;
+    let unicode = 57344;
     let metadata = {};
     if (fs.existsSync("metadata.json")) {
         metadata = JSON.parse(fs.readFileSync("metadata.json", "utf8"));
-        unicode = Math.max.apply(Object.keys(metadata).map(item => item.unicode));
+        unicode = Math.max.apply(Object.keys(metadata).map(item => item.unicode)) + 1;
     }
     const bufferContents = function (file, enc, callback) {
         const name = path.basename(file.path, ".svg");
@@ -36,7 +51,7 @@ const generateMetadata = () => {
             metadata[name] = {
                 name: name,
                 keywords: [],
-                unicode: unicode + 1,
+                unicode: unicode,
             };
             unicode = unicode + 1;
         }
@@ -60,7 +75,7 @@ const iconsToJson = () => {
         const name = path.basename(file.path, ".svg");
         icons[name] = {
             name: name,
-            path: /\sd="([\w,\.\-\s]*)"/gm.exec(content)[1] || "",
+            path: getIconPath(content),
         };
         lastFile = file; // Save reference to last file
         return callback();
@@ -96,8 +111,27 @@ gulp.task("build:json", () => {
 });
 
 gulp.task("build:react", () => {
-    // return gulp.src("icons/*.svg")
-    //     .pipe(iconsToJson())
-    //     .pipe(gulp.dest("packages/json/"));
+    return gulp.src("icons/*.svg")
+        .pipe(iconsToJson())
+        .pipe(through.obj(function (file, enc, callback) {
+            const source = fs.readFileSync(".build/index-react.hbs", "utf8");
+            const template = hbs.compile(source);
+            const content = template({
+                icons: JSON.parse(file.contents.toString()),
+                version: pkg.version,
+            });
+
+            this.push(new Vinyl({
+                path: path.join(process.cwd(), "index.js"),
+                contents: new Buffer.from(content),
+            }));
+            return callback();
+        }))
+        .pipe(babel({
+            plugins: [
+                "@babel/plugin-transform-react-jsx",
+            ],
+        }))
+        .pipe(gulp.dest("packages/react/"));
 });
 
