d5686028b904bd7566cd9781d91bbfe224e16fca
Josemi <josemijuanes@gmail.com>
2023-07-05 19:02:04 +0200

    Updated tests

--- packages/react/test.js
+++ packages/react/test.js
@@ -1,21 +1,17 @@
 import TestRenderer from "react-test-renderer";
 
-import * as Mochicons from "./index.js";
+import * as Icons from "./index.esm.js";
 
-describe("@mochicons/react", () => {
-    it("should export all icons as a react components", () => {
-        expect(Object.keys(Mochicons)).toMatchSnapshot();
-    });
-
+describe("@josemi-icons/react", () => {
     it("should wrap icons in a <svg> component", () => {
-        const iconInstance = TestRenderer.create(<Mochicons.Icon />);
+        const iconInstance = TestRenderer.create(<Icons.Icon />);
 
         expect(iconInstance.root.findByType("svg").props.width).toEqual(expect.stringContaining("1em"));
         expect(iconInstance.root.findByType("svg").props.height).toEqual(expect.stringContaining("1em"));
     });
 
     it("should wrap icons in a <g> component", () => {
-        const iconInstance = TestRenderer.create(<Mochicons.Icon />);
+        const iconInstance = TestRenderer.create(<Icons.Icon />);
 
         expect(iconInstance.root.findByType("g").props.stroke).toEqual(expect.stringContaining("currentColor"));
         expect(iconInstance.root.findByType("g").props.strokeWidth).toEqual(expect.stringContaining("2"));
