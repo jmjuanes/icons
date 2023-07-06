dc9c283863df093b2a6b9af85fa14b6959b1f95e
Josemi <josemijuanes@gmail.com>
2023-07-06 20:04:06 +0200

    docs: added usage page and some updates in layout

--- scripts/docs.js
+++ scripts/docs.js
@@ -6,7 +6,7 @@ const runtime = require("react/jsx-runtime");
 const matter = require("gray-matter");
 const hljs = require("highlight.js/lib/common");
 
-const {Icon} = require("../packages/react/index.cjs.js");
+const {Icon, BarsIcon} = require("../packages/react/index.cjs.js");
 
 const pkg = require("../package.json");
 const icons = require("../icons.json");
@@ -26,12 +26,31 @@ const CodeBlock = props => {
     );
 };
 
+const NavbarLink = props => (
+    <a href={props.to} className="flex items-center gap-2 text-gray-800 px-3 py-2 rounded-md hover:bg-gray-200 no-underline">
+        {props.icon && (
+            <div className="flex items-center text-xl">
+                <Icon icon={props.icon} />
+            </div>
+        )}
+        <div className="font-medium">
+            {props.text || props.children}
+        </div>
+    </a>
+);
+
 const pageComponents = {
-    "h1": props => <h1 className="mb-4 text-gray-800 text-2xl font-bold">{props.children}</h1>,
-    "h2": props => <h2 className="mb-4 text-gray-800 text-xl font-bold">{props.children}</h2>,
+    "h1": props => <h1 className="mt-8 mb-4 text-gray-800 text-2xl font-bold">{props.children}</h1>,
+    "h2": props => <h2 className="mt-8 mb-4 text-gray-800 text-xl font-bold">{props.children}</h2>,
     "p": props => <p className="mt-6 mb-6">{props.children}</p>,
+    "a": props => (
+        <a {...props} className={props.className || `underline text-gray-900 hover:text-gray-700`}>
+            {props.children}
+        </a>
+    ),
     "code": props => <code className="font-bold text-sm font-mono">{`'`}{props.children}{`'`}</code>,
     Icon: props => <Icon icon={props.icon} />,
+    Separator: () => <div className="w-full h-px bg-gray-200 my-10" />,
     CodeBlock: CodeBlock,
     Fragment: React.Fragment,
 };
@@ -64,8 +83,8 @@ const PageWrapper = props => (
         </head>
         <body className="bg-white m-0 p-0 font-inter text-gray-800 leading-normal">
             {/* Header */}
-            <div className="border-b-1 border-gray-300 relative">
-                <div className="w-full maxw-7xl h-20 px-6 mx-auto flex items-center justify-between">
+            <div className="border-b-1 border-gray-300">
+                <div className="w-full maxw-7xl h-16 px-6 mx-auto flex items-center justify-between">
                     <a href="./index.html" className="flex items-center gap-2 text-gray-800 no-underline">
                         <div className="font-bold flex items-center">
                             <span>josemi/</span>
@@ -75,16 +94,43 @@ const PageWrapper = props => (
                             <span>v{pkg.version}</span>
                         </div>
                     </a>
-                    <div className="flex flex-row gap-6 items-center">
-                        <a href={pkg.repository} className="no-underline o-70 hover:o-100">
-                            <img className="w-6 h-6" src="./github.svg" />
-                        </a>
+                    <div className="group peer" tabIndex="0">
+                        <div className="flex sm:hidden text-xl p-2 border border-gray-300 rounded-md cursor-pointer">
+                            <BarsIcon />
+                        </div>
+                        <div className="fixed sm:initial top-0 right-0 p-6 sm:p-0 hidden sm:block group-focus-within:block z-5">
+                            <div className="flex flex-col sm:flex-row gap-3 sm:items-center rounded-md bg-white p-4 sm:p-0 w-72 sm:w-auto">
+                                <div className="pr-12 sm:pr-0 sm:flex sm:gap-3">
+                                    <NavbarLink to="./usage.html" text="Usage" icon="book-open" />
+                                </div>
+                                <div className="h-px w-full sm:h-8 sm:w-px bg-gray-300" />
+                                <div className="flex">
+                                    <NavbarLink to={pkg.repository.url}>
+                                        <img className="w-6 h-6" src="./github.svg" />
+                                    </NavbarLink>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    <div className="fixed top-0 left-0 w-full h-full sm:h-0 peer-focus-within:block hidden sm:hidden bg-gray-900 o-60 z-2" />
                 </div>
             </div>
             {/* Main content */}
             <div className="w-full maxw-7xl mx-auto px-6">
-                {props.element}
+                {props.page?.data?.layout === "page" && (
+                    <div className="mx-auto maxw-4xl w-full">
+                        <div className="my-10">
+                            <div className="text-5xl font-black">{props.page.data.title}</div>
+                            <div className="mt-0 text-lg text-gray-500 font-medium leading-relaxed">{props.page.data.description}</div>
+                        </div>
+                        {props.element}
+                    </div>
+                )}
+                {props.page?.data?.layout === "default" && (
+                    <React.Fragment>
+                        {props.element}
+                    </React.Fragment>
+                )}
             </div>
             {/* Footer */}
             <div className="w-full maxw-7xl mx-auto px-6 pt-10 pb-20">
