15e5464c9c6931003429b1a32bec6ec15ac9f017
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-12-30 22:44:53 +0000

    Add minify script

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,5 +1,12 @@
+const CleanCSS = require("clean-css");
+
+// minify the provided css string
+module.exports.minifyCss = (css = "", options = {}) => {
+    return new CleanCSS(options).minify(css);
+};
+
 // generate an svg string with the provided icon path
-module.generateSvg = (p, size = "1em", separator = "") => {
+module.exports.generateSvg = (p, size = "1em", separator = "") => {
     const items = [
 		`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="${size}" height="${size}">`,
 		`<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="${p}"/>`,
@@ -10,7 +17,7 @@ module.generateSvg = (p, size = "1em", separator = "") => {
 
 // encode SVG for using in CSS
 // based on https://bl.ocks.org/jennyknuth/222825e315d45a738ed9d6e04c7a88d0
-module.encodeSvg = str => {
+module.exports.encodeSvg = str => {
 	return str
 		.replace(/"/g, "'")
 		.replace(/%/g, "%25")
