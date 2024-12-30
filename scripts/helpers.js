7f01cac4d5483ff34d950cf51c47859fd7745024
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-12-30 20:05:10 +0100

    Add helpers

---
+++ scripts/helpers.js
@@ -0,0 +1,20 @@
+// generate an svg string with the provided icon path
+module.generateSvg = (p, size = "1em", separator = "") => {
+    const items = [
+		`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="${size}" height="${size}">`,
+		`<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="${p}"/>`,
+		`</svg>`
+	];
+    return items.join(separator);
+};
+
+// encode SVG for using in CSS
+// based on https://bl.ocks.org/jennyknuth/222825e315d45a738ed9d6e04c7a88d0
+module.encodeSvg = str => {
+	return str
+		.replace(/"/g, "'")
+		.replace(/%/g, "%25")
+		.replace(/#/g, "%23")
+		.replace(/</g, "%3C")
+		.replace(/>/g, "%3E");
+};
