27c37f47bd51d8eb2719c3497a0ef3fa6c35b88e
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-12-31 17:21:36 +0100

    Fix build icons script

--- scripts/build.js
+++ scripts/build.js
@@ -1,32 +1,22 @@
-const fs = require("node:fs/promises");
+const fs = require("node:fs");
 const path = require("node:path");
+const {generateSvg} = require("./helpers.js");
 const {version} = require("../package.json");
 
 // Tiny utility to extract the d="" segment from the icon
 const getIconPath = str => str.match(/\sd="([^\"]*)"/im)[1];
 
-// Generate the icon content
-const getIconContent = (p, separator = "") => {
-    const iconContent = [
-        `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em">`,
-        `<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="${p}"/>`,
-        `</svg>`,
-    ];
-    return iconContent.join(separator);
-};
-
 // Generate icon SVG from path
-const generateSvg = async icons => {
+const generateIconsImages = icons => {
     for (let i = 0; i < icons.length; i++) {
         const icon = icons[i];
         const iconPath = path.join(process.cwd(), "icons", `${icon.name}.svg`);
-        const iconContent = getIconContent(icon.path, "\n");
-        await fs.writeFile(iconPath, iconContent, "utf-8");
+        const iconContent = generateSvg(icon.path, "1em", "\n");
+        fs.writeFileSync(iconPath, iconContent, "utf-8");
     }
-    return true;
 };
 
-const generateJson = icons => {
+const generateIconsJson = icons => {
     const iconsPath = path.join(process.cwd(), "icons.json");
     const iconsJson = {
         "$id": "./icons.schema.json",
@@ -34,12 +24,12 @@ const generateJson = icons => {
         "icons": icons,
     };
     // Save icons to JSON file
-    return fs.writeFile(iconsPath, JSON.stringify(iconsJson, null, "    "), "utf8");
+    return fs.writeFileSync(iconsPath, JSON.stringify(iconsJson, null, "    "), "utf8");
 };
 
-const main = async () => {
+const build = () => {
     const srcFolder = path.join(process.cwd(), "src");
-    const iconsFiles = await fs.readdir(srcFolder);
+    const iconsFiles = fs.readdirSync(srcFolder);
     const icons = [];
     // Generate icons in JSON format
     for (let i = 0; i < iconsFiles.length; i++) {
@@ -47,7 +37,7 @@ const main = async () => {
         if (path.extname(icon) === ".svg") {
             const name = path.basename(icon, ".svg");
             const iconPath = path.join(srcFolder, icon);
-            const iconContent = await fs.readFile(iconPath, "utf8");
+            const iconContent = fs.readFileSync(iconPath, "utf8");
             // Insert icon object
             icons.push({
                 name: name,
@@ -56,9 +46,9 @@ const main = async () => {
         }
     }
     // Generate SVG and JSON
-    await generateSvg(icons);
-    await generateJson(icons);
+    generateIconsImages(icons);
+    generateIconsJson(icons);
 };
 
 // Build icons
-main();
+build();
