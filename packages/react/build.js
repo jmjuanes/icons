fe42d8fa7cf568268b9b714d7db045469cc9ecc3
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-12-31 16:14:10 +0100

    Fix react build script

--- packages/react/build.js
+++ packages/react/build.js
@@ -1,3 +1,4 @@
+const fs = require("node:fs");
 const babel = require("@babel/core");
 const {icons} = require("../../icons.json");
 
@@ -6,52 +7,40 @@ const pascalCase = str => {
     return str.match(/[a-zA-Z0-9]+/g).map(w => `${w.charAt(0).toUpperCase()}${w.slice(1)}`).join("");
 };
 
-module.exports = {
-    output: {
-        "esm": {
-            type: "module",
-        },
-        "cjs": {
-            type: "commonjs2",
-        },
-        "umd": {
-            type: "umd",
-            name: "Icons",
-        },
-    },
-    externals: {
-        react: "react",
-    },
-    generateEntry: () => {
-        const code = [
-            `import React from "react";`,
-            `export const Icon = props => (`,
-            `    <svg xmlns="http://www.w3.org/2000/svg" width={props.size} height={props.size} viewBox="0 0 24 24">`,
-            `        <g fill="none" stroke={props.color} strokeLinecap="round" strokeLinejoin="round" strokeWidth={props.stroke}>`,
-            `            <path d={props.path || ""} />`,
-            `        </g>`,
-            `    </svg>`,
-            `);`,
-            `Icon.defaultProps = {`,
-            `    path: "",`,
-            `    size: "1em",`,
-            `    color: "currentColor",`,
-            `    stroke: 2,`,
-            `};`,
-            ...icons.map(icon => {
-                return `export const ${pascalCase(icon.name)}Icon = props => (<Icon {...props} path={"${icon.path}"} />);`;
-            }),
-            `const ICONS = {`,
-            ...icons.map(icon => {
-                return `"${icon.name}": ${pascalCase(icon.name)}Icon,`;
-            }),
-            `};`,
-            `export const renderIcon = name => React.createElement(ICONS[name], {});`,
-        ];
-        return babel.transformSync(code.join("\n"), {
-            presets: [
-                "@babel/preset-react",
-            ],
-        });
-    },
+const build = () => {
+    const code = [
+        `import React from "react";`,
+        `export const Icon = ({size = "1em", color = "currentColor", stroke = "2", path = ""}) => (`,
+        `    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24">`,
+        `        <g fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth={stroke}>`,
+        `            <path d={path} />`,
+        `        </g>`,
+        `    </svg>`,
+        `);`,
+        // `Icon.defaultProps = {`,
+        // `    path: "",`,
+        // `    size: "1em",`,
+        // `    color: "currentColor",`,
+        // `    stroke: 2,`,
+        // `};`,
+        ...icons.map(icon => {
+            return `export const ${pascalCase(icon.name)}Icon = props => (<Icon {...props} path={"${icon.path}"} />);`;
+        }),
+        `const ICONS = {`,
+        ...icons.map(icon => {
+            return `"${icon.name}": ${pascalCase(icon.name)}Icon,`;
+        }),
+        `};`,
+        `export const renderIcon = name => React.createElement(ICONS[name], {});`,
+    ];
+    // convert jsx to react.createElement calls
+    const output = babel.transformSync(code.join("\n"), {
+        presets: [
+            "@babel/preset-react",
+        ],
+    });
+    // write output
+    fs.writeFileSync("./index.js", output, "utf8");
 };
+
+build();
