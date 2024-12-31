f50b6f41312a8296457f1a8b1e3f87a096ce8b6a
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-12-31 16:47:14 +0100

    Move icons styles template to build

--- packages/css/build.js
+++ packages/css/build.js
@@ -1,19 +1,47 @@
 const fs = require("node:fs");
 const {generateSvg, encodeSvg, minifyCss} = require("../../scripts/helpers.js");
-const icons = require("../../icons.json");
+const {icons} = require("../../icons.json");
 
-const build = async () => {
-    const mikel = (await import("mikel")).default;
-    const template = fs.readFileSync("../../templates/icons.css.mustache", "utf8");
-    const content = mikel(template, icons, {
-        functions: {
-            svg: args => {
-                return encodeSvg(generateSvg(args.opt.path));
-            },
-        },
-    });
+const build = () => {
+    const separator = "\n";
+    const css = [
+        `:root,`,
+        `*:before {`,
+        ...icons.map(icon => {
+            const path = encodeSvg(generateSvg(icon.path));
+            return `--josemi-icons-${icon.name}: url("data:image/svg+xml;utf8,${path}") no-repeat;`;
+        }),
+        `}`,
+        `[class^="ji-"],`,
+        `[class*=" ji-"] {`,
+        `    align-self: center;`,
+        `    display: inline-flex;`,
+        `    line-height: 1;`,
+        `    text-rendering: auto;`,
+        `    vertical-align: -0.125em;`,
+        `}`,
+        `[class^="ji-"]:before,`,
+        `[class*=" ji-"]:before {`,
+        `    content: "''";`,
+        `    background-color: currentColor;`,
+        `    display: inline-block;`,
+        `    width: 1em;`,
+        `    height: 1em;`,
+        `}`,
+        ...icons.map(icon => {
+            const iconStyles = [
+                `.ji-{{name}}:before {`
+                `    mask: var(--josemi-icons-${icon.name}) no-repeat;`,
+                `    mask-size: 100% 100%;`,
+                `    -webkit-mask: var(--josemi-icons-${icon.name}) no-repeat;`,
+                `    -webkit-mask-size: 100% 100%;`,
+                `}`,
+            ];
+            return iconStyles.join(separator);
+        }),
+    ];
     // minify the css
-    const output = minifyCss(content, {
+    const output = minifyCss(css.join(separator), {
         level: 2,
         compatibility: "*",
     });
