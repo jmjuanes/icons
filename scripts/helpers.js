85bfa68fa9fe783eb873ca031177db2887373e70
Josemi <josemijuanes@gmail.com>
2025-03-31 19:09:07 +0200

    Migrate scripts to esm

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,12 +1,12 @@
-const CleanCSS = require("clean-css");
+import CleanCSS from "clean-css";
 
 // minify the provided css string
-module.exports.minifyCss = (css = "", options = {}) => {
+export const minifyCss = (css = "", options = {}) => {
     return new CleanCSS(options).minify(css);
 };
 
 // generate an svg string with the provided icon path
-module.exports.generateSvg = (p, size = "1em", separator = "") => {
+export const generateSvg = (p, size = "1em", separator = "") => {
     const items = [
 		`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="${size}" height="${size}">`,
 		`<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="${p}"/>`,
@@ -17,7 +17,7 @@ module.exports.generateSvg = (p, size = "1em", separator = "") => {
 
 // encode SVG for using in CSS
 // based on https://bl.ocks.org/jennyknuth/222825e315d45a738ed9d6e04c7a88d0
-module.exports.encodeSvg = str => {
+export const encodeSvg = str => {
 	return str
 		.replace(/"/g, "'")
 		.replace(/%/g, "%25")
--- scripts/release.js
+++ scripts/release.js
@@ -1,32 +1,31 @@
-const fs = require("node:fs/promises");
-const path = require("path");
+import * as fs from "node:fs";
+import * as path from "node:path";
+import pkg from "../package.json" with {type: "json"};
 
-const {version} = require("../package.json");
-
+// read a json file
 const readJson = file => {
-    return fs.readFile(file, "utf8")
-        .then(data => JSON.parse(data));
+    return JSON.parse(fs.readFileSync(file, "utf8"));
 };
 
+// write the json file
 const writeJson = (file, content) => {
-    return fs.writeFile(file, JSON.stringify(content, null, "    "), "utf8");
+    return fs.writeFileSync(file, JSON.stringify(content, null, "    "), "utf8");
 };
 
-// Prepare next release
-const main = async () => {
+// prepare next release
+const main = () => {
     console.log("[release] Prepare release for packages in './packages' folder");
     const packagesFolder = path.join(process.cwd(), "packages");
-    const packagesList = await fs.readdir(packagesFolder);
+    const packagesList = fs.readdirSync(packagesFolder);
     console.log(`[release] Found ${packagesList.length} packages to prepare`);
     // Prepare each package in ./packages folder
-    for (let i = 0; i < packagesList.length; i++) {
-        const name = packagesList[i];
+    packagesList.forEach(name => {
         console.log(`[release] Preparing package '${name}'...`);
         const packagePath = path.join(packagesFolder, name, "package.json");
-        const currentPkg = await readJson(packagePath);
-        currentPkg.version = version;
-        await writeJson(packagePath, currentPkg);
-    }
+        const currentPkg = readJson(packagePath);
+        currentPkg.version = pkg.version;
+        writeJson(packagePath, currentPkg);
+    });
     // Finish release prepare script
     console.log("[release] Release ready");
 };
