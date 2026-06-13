0858cd1675b9629a62c23e0d3d5f6bad17f8ee23
Josemi <josemijuanes@gmail.com>
2026-06-13 16:47:35 +0000

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
-/** @type {import("jest").Config} */
 export default {
+    preset: "ts-jest",
     testEnvironment: "jsdom",
-    transform: {},
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/$1",
+        "^(\\.{1,2}/.*)\\.(js|tsx)$": "$1",
+    },
+    setupFilesAfterEnv: [
+        "@testing-library/jest-dom"
+    ],
+    extensionsToTreatAsEsm: [".ts", ".tsx"],
+    moduleFileExtensions: ["tsx", "ts", "js", "jsx", "json", "node"],
 };
